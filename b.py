48d54fe7d5f2b46ae99dde0c9d1640c0b332c369
shivanggupta1509 <87229803+shivanggupta1509@users.noreply.github.com>
2022-08-30 19:55:20 +0530

    Update b.py

--- b.py
+++ b.py
@@ -53,7 +53,7 @@ class Main:
 		username                   = string.ascii_letters + string.digits
 		username                   = ('Shivang')
 		credentails['username']    = username
-		credentails['email']       = self.email+"dripplay.tk
+		credentails['email']       = self.email+"dripplay.tk"
 
 		return credentails
 
